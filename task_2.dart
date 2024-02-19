8e6393a23154555626ccf03a5cbfad96316f36d5
Bogdan <bogdan-dev@bk.ru>
2024-02-19 15:24:30 +0300

    The solution to task number 2 has been changed

--- task_2.dart
+++ task_2.dart
@@ -1,7 +1,6 @@
 double getAverageArithmetics(List<int> listNumbers) {
-  double average = 0.0;
-  listNumbers.forEach((number) => average += number);
-  return average / listNumbers.length;
+  return listNumbers.reduce((acum, number) => acum + number) /
+      listNumbers.length;
 }
 
 final array = [1, 2, 3, 4, 5, 6];
